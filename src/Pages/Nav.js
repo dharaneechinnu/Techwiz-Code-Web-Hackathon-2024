c8cec2f2d5bc304c57f7ed0b1bb94c929ff4d8c2
dharaneedharan <117936700+dharaneechinnu@users.noreply.github.com>
2024-04-12 15:19:38 +0530

    origin

--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import logo from '../Pages/img/club.png';
-import '../Pages/Nav.css';
-
+import React from 'react'
+import logo from '../Pages/img/club.png'
+import '../Pages/Nav.css'
 const Nav = () => {
   return (
-    <div className="nav">
+    <>
+      <div className="nav">
+     
       <ul>
-        <img src={logo} alt="" />
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Past Events</a></li>
-        <li><a href="#">Gallery</a></li>
+      <img src={logo} alt=""  />
+        <li>Home</li>
+        <li>About</li>
+        <li>Past Events</li>
+        <li>gallery</li>
+       
       </ul>
-    </div>
-  );
-};
+      </div>
+    </>
+  )
+}
 
-export default Nav;
+export default Nav
